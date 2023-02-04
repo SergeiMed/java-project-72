7f43ea2a46263ee2efa4d06a0060151ae035a0e5
SergeiMed <s.medvedev86@mail.ru>
2023-02-04 17:26:20 +0300

    add Javalin

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,24 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
 
-    public static void main(String[] args) {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.valueOf(port);
+    }
 
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
